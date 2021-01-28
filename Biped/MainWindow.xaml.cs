b445928ff9f303f0344e6e78e3e71de95d3eecf3
Tom Fairfield <tom@fairfieldt.com>
2021-01-27 17:46:49 -0800

    adds single instance - only 1 Biped will be run.

--- Biped/MainWindow.xaml.cs
+++ Biped/MainWindow.xaml.cs
@@ -2,11 +2,11 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 
 using System.Windows.Input;
-
-
+using System.Windows.Interop;
 
 namespace biped
 {
@@ -48,16 +48,57 @@ namespace biped
         private Config config;
 
         private Pedal currentPedalToSet = Pedal.NONE;
+
+        static Mutex mutex = new Mutex(true, "Biped-SingleInstance");
+
+        public const int HWND_BROADCAST = 0xffff;
+        public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
+        [DllImport("user32")]
+        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+        [DllImport("user32")]
+        public static extern int RegisterWindowMessage(string message);
+        [DllImport("user32")]
+        public static extern IntPtr FindWindow(string classname, string windowname);
         public MainWindow()
         {
-            InitializeComponent();
-            LoadPedalBinds();
-            biped = new Biped(config);
-            if (biped.DeviceConnected != true)
+            if (mutex.WaitOne(TimeSpan.Zero, true))
             {
-                StatusText.Content = NO_DEVICE_DETECTED_TEXT;
+                InitializeComponent();
+                LoadPedalBinds();
+                biped = new Biped(config);
+                if (biped.DeviceConnected != true)
+                {
+                    StatusText.Content = NO_DEVICE_DETECTED_TEXT;
+                }
+            }
+            else
+            {
+                IntPtr hwnd = FindWindow(null, "Biped");
+                if (hwnd != IntPtr.Zero)
+                {
+                    PostMessage(hwnd, WM_SHOWME, IntPtr.Zero, IntPtr.Zero);
+                }
+                Application.Current.Shutdown();
+
             }
         }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+
+        }
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_SHOWME)
+            {
+                Show();
+            }
+            return IntPtr.Zero;
+        }
+
 
         private void RecordPedalBind(Pedal pedal)
         {
